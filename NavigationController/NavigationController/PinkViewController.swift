39f2ed46b1b2025a3a47f8547b916b7651e6ee7d
Matheus B. Grigoletto <matheus.grigoletto@me.com>
2020-11-02 12:47:59 -0300

    exercicio navigation controller (youtube)

---
+++ NavigationController/NavigationController/PinkViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PinkViewController.swift
+//  NavigationController
+//
+//  Created by Matheus B. Grigoletto on 26/10/20.
+//  Copyright © 2020 Matheus B. Grigoletto. All rights reserved.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Pink Screen"
+
+        print("Pink DidLoad")
+    }
+
+}
---
+++ NavigationController/NavigationController/TealViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Matheus B. Grigoletto on 26/10/20.
+//  Copyright © 2020 Matheus B. Grigoletto. All rights reserved.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Teal Screen"
+        
+        print("Teal DidLoad")
+    }
+
+
+}
+
